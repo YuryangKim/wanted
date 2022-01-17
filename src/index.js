c9ecab52450b692cb8720e7913b108f300791756
yuryang Kim <flowratekim@gmail.com>
2022-01-18 00:45:06 +0900

    Add: images, Nav.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Nav from './components/Nav';
 
-ReactDOM.render(
-    <App />
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Nav />, document.getElementById('root'));
